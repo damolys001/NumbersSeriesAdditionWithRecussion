7d696eb4256b4eb3f0788d5cc9fc4a400c55e97b
Adedamola Adeyemi <adedamola.adeyemi@live.com>
2023-07-07 23:48:42 -0700

    Add project files.

---
+++ AdditionRecussion/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Add series of numbe with recusion...");
+
+
+Console.WriteLine(AddNumbers(10));
+
+Console.ReadLine();
+ int AddNumbers(int number)
+{ 
+    if (number <= 0)
+        return 0;
+
+    return number + AddNumbers(number - 1);
+}
